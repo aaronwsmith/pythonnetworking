4748004b9d34565612853ce3ab8dce5a80beebe3
Aaron Smith <aaronwsmith@gmail.com>
2025-04-22 10:12:20 -0500

    Minor enhancement to use the errno and os modules to interpet the error codes from connect_ex()

--- portscan.py
+++ portscan.py
@@ -1,4 +1,6 @@
-from socket import socket, AF_INET, SOCK_STREAM
+import errno
+import os
+from   socket import socket, AF_INET, SOCK_STREAM
 
 ip       = '127.0.0.1'
 portlist = [ 22, 80, 443 ]
@@ -7,6 +9,12 @@ for port in portlist:
     sock    = socket( AF_INET, SOCK_STREAM )
     pstatus = sock.connect_ex( (ip,port) )
 
-    print( f'{ip}: port {port} status: {pstatus}' )
+    if pstatus == 0:
+        pstatus = 'OPEN: Connected'
+    else:
+        _pstatus = errno.errorcode.get( pstatus, 'Unknown error' )
+        pstatus  = f'{_pstatus}: {os.strerror(pstatus)}'
+
+    print( f'{ip}: port {port}: {pstatus}' )
 
     sock.close()
