7ae229ce9662a500758c6b4d4d8ed2dc26f632a4
Aaron Smith <aaronwsmith@gmail.com>
2025-07-30 09:18:04 -0500

    Added a docstring with more information and source material for future reference.

--- portscan.py
+++ portscan.py
@@ -1,3 +1,21 @@
+'''
+This script performs a port scan against the specified host, defaulting to the localhost. Based on code provided by the
+book "Python for Security and Networking" by Jose Manuel Ortega, Chapter 3 page 88. 
+
+TCP Sockets: SOCK_STREAM
+UDP Sockets: SOCK_DGRAM
+
+IPv4: AP_INET
+IPv6: AP_INET6
+
+errno: This module makes available standard errno system symbols. The value of each symbol is the corresponding integer value.
+https://docs.python.org/3/library/errno.html
+
+socket: This module provides access to the BSD socket interface. It is available on all modern Unix systems, Windows, MacOS,
+and probably additional platforms.
+https://docs.python.org/3/library/socket.html
+'''
+
 import errno
 import os
 from   socket import socket, AF_INET, SOCK_STREAM
